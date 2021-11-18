d6ca9914d1988a73ab59182b8731f9a9947881a6
tabinoki <k.tabinoki961027@gmail.com>
2021-11-18 10:07:06 +0900

    デザイン修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -9,14 +9,37 @@ class TopController < ApplicationController
     end
 
     def result
+        # 入力された値をそれぞれ変数に代入
         year = params["date(1i)"].to_i
         month = params["date(2i)"].to_i
         day = params["date(3i)"].to_i
         @city = params[:city]
+        # 都市名から緯度経度を取得して変数に代入
         geocode = Geocoder.search(@city)
         lat = geocode.first.coordinates[0]
         lng = geocode.first.coordinates[1]
-        @time = calc_sunset(year, month, day, lat, lng)
+
+        # それぞれの値をURIの形式に変換し、変数paramsに代入
+        @rise_set_params= URI.encode_www_form(
+        { mode: "sun_moon_rise_set",
+        year: year,
+        month: month,
+        day: day,
+        lat: lat,
+        lng: lng }
+        )
+        # APIを叩く
+        rise_set_uri = URI.parse("https://labs.bitmeister.jp/ohakon/json/?#{@rise_set_params}")
+        rise_set_json = Net::HTTP.get_response(rise_set_uri)
+        
+        rise_set_result = JSON.parse(rise_set_json.body)
+
+        @sunrise_hm = rise_set_result["rise_and_set"]["sunrise_hm"]
+        sunset = rise_set_result["rise_and_set"]["sunset"]
+        @sunset_hm = rise_set_result["rise_and_set"]["sunset_hm"]
+        @rise_time = time_label(@sunrise_hm)
+        @set_time = time_label(@sunset_hm)
+        
         @thi_ago = ago(30)
         @thi_late = late(30)
         @date = "#{year}年#{month}月#{day}日"
@@ -31,29 +54,17 @@ class TopController < ApplicationController
     def terms
     end
 
-    
-    def calc_sunset(year, month, day, lat, lng)
-        @params= URI.encode_www_form(
-        { mode: "sun_moon_rise_set",
-        year: year,
-        month: month,
-        day: day,
-        lat: lat,
-        lng: lng }
-        )
-        uri = URI.parse("https://labs.bitmeister.jp/ohakon/json/?#{@params}")
-        json = Net::HTTP.get_response(uri)
-        result = JSON.parse(json.body)
-        if result["rise_and_set"]
-            @sunset_hm = result["rise_and_set"]["sunset_hm"]
-            h, m = @sunset_hm.split(":").map(&:to_i)
-            m = m.to_s
+    def time_label(rise_set_time)
+        h, m = rise_set_time.split(":").map(&:to_i)
+        h = h.to_s
+        m = m.to_s
+            if h.size == 1
+                h = "0" + h
+            end
             if m.size == 1
                 m = "0" + m
             end
-            "#{h}時#{m}分"
-        end
-        
+        "#{h}時#{m}分"
     end
 
     def ago(ago_m)
