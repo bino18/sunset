af542ac4a6313f5aef9b7c5a50d9fc3cf5fc324a
tabinoki <k.tabinoki961027@gmail.com>
2022-01-04 16:47:14 +0900

    ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/',  to: "top#top"
+  get '/datail', to: "top#detail"
   get '/result',  to: "top#result"
   get '/privacy', to: "top#privacy"
   get '/support', to: "top#support"
